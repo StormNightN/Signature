7d27f9b7713038002ec0ca5e34df26ad22d08060
stormnight <stormnight@gmail.com>
2019-09-04 18:02:46 +0200

    FileReader is prepated to pushing to workqueue

--- FileReader.cpp
+++ FileReader.cpp
@@ -2,3 +2,31 @@
 // Created by stormnight on 9/4/19.
 //
 
+#include <fstream>
+#include <memory>
+#include "FileReader.h"
+
+Signature::FileReader::FileReader(std::string path, size_t blockSize) :
+    m_Path(std::move(path)),
+    m_BlockSize(blockSize * 1024 *1024) {
+
+}
+
+void Signature::FileReader::Read() const{
+    std::ifstream inputFile;
+    inputFile.open(m_Path, std::ios::in |std::ios::binary);
+
+    if(inputFile.is_open()) {
+
+        while (inputFile) {
+            auto pDataBuffer = std::make_unique<unsigned char[]>(m_BlockSize);
+            for(auto idx = inputFile.gcount(); idx < m_BlockSize; idx++) {
+                pDataBuffer[idx] = 0U;
+            }
+            // TODO push to work queue
+        }
+
+    } else {
+        throw std::ifstream::failure("Can't open file " + m_Path);
+    }
+}
--- FileReader.h
+++ FileReader.h
@@ -5,4 +5,36 @@
 #ifndef SIGNATURE_FILEREADER_H
 #define SIGNATURE_FILEREADER_H
 
+#include <string>
+
+
+namespace Signature {
+
+
+    /**
+     * Represent object, which will read file and push DataChunks to WorkQueue
+     */
+    class FileReader {
+
+    public:
+        /**
+         * Default constructor
+         *
+         * @param path              path to file
+         * @param blockSize         block size, which will be pushed to queue
+         */
+        explicit FileReader(std::string path, size_t blockSize = 1);
+
+        /**
+         * Reades blocks and pushes to queue
+         */
+        void Read() const;
+    private:
+
+        std::string m_Path;
+
+        size_t m_BlockSize;
+    };
+}
+
 #endif //SIGNATURE_FILEREADER_H
