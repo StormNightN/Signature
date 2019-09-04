7e8204a95db90970817e22c3472139da148ff856
stormnight <stormnight@gmail.com>
2019-09-04 19:47:10 +0200

    Add HashProcess class

--- HashProcess.cpp
+++ HashProcess.cpp
@@ -1,4 +1,21 @@
 //
 // Created by stormnight on 9/4/19.
 //
+#include "HashProcess.h"
 
+Signature::HashProcess::HashProcess(Signature::WorkQueue<Signature::DataChank> &rWorkQueue) :
+    m_WorkQueue(rWorkQueue) {
+
+}
+
+bool Signature::HashProcess::Process() {
+    auto pDataChank = m_WorkQueue.Pop();
+    if(pDataChank != nullptr) {
+        pDataChank->Hash();
+        // TODO push to result queue
+
+        return true;
+    } else {
+        return false;
+    }
+}
--- HashProcess.h
+++ HashProcess.h
@@ -5,4 +5,45 @@
 #ifndef SIGNATURE_HASHPROCESS_H
 #define SIGNATURE_HASHPROCESS_H
 
+#include "WorkQueue.h"
+
+namespace Signature {
+
+    /**
+     * Represent Hash process action
+     */
+    class HashProcess {
+
+    public:
+
+        /**
+         * Default constructor
+         *
+         * @param rWorkQueue reference to work queue
+         */
+        explicit HashProcess(WorkQueue<DataChank>& rWorkQueue);
+
+        /**
+         * Calculate hash for next element from work queue and
+         * push it to result queue
+         *
+         * @return processing result
+         * @retval true new chank was processed
+         * @retval false work queue is empty
+         */
+        bool Process();
+
+        /**
+         * Destructor
+         */
+        virtual ~HashProcess() = default;
+
+    private:
+
+        /// Reference to work queue
+        ///
+        WorkQueue<DataChank>& m_WorkQueue;
+    };
+}
+
 #endif //SIGNATURE_HASHPROCESS_H
