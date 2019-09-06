b3266d2f7bd7150809d3064c02b76689a6843908
stormnight <stormnight@gmail.com>
2019-09-06 18:00:34 +0200

    Add FileWriter first implementation

--- FileWriter.cpp
+++ FileWriter.cpp
@@ -1,4 +1,44 @@
 //
 // Created by stormnight on 9/6/19.
 //
+#include <fstream>
+#include "FileWriter.h"
 
+
+Signature::FileWriter::FileWriter(size_t hashCount, std::string path) :
+        m_Path(std::move(path)),
+        m_HashData(hashCount, nullptr),
+        m_WriteMutex()
+{
+
+}
+
+void Signature::FileWriter::ProcessHash() {
+    size_t hashId = 0U;
+    size_t hashCount = m_HashData.size();
+    std::ofstream outputFile(m_Path);
+
+    while (hashId < hashCount) {
+        std::unique_ptr<DataChank> p_ProcessingChank(nullptr);
+        do {
+            std::unique_lock<std::mutex> lock(m_WriteMutex);
+            std::swap(p_ProcessingChank, m_HashData[hashId]);
+
+            if(p_ProcessingChank == nullptr) {
+                // TODO signalization
+            }
+        } while (p_ProcessingChank == nullptr); //while waited chank is not pushed
+
+        PrintToOutput(outputFile, p_ProcessingChank, hashId++);
+    }
+}
+
+void Signature::FileWriter::PrintToOutput(std::ostream& os,
+        std::unique_ptr<DataChank>& rp_ProcessingChank, size_t idx) {
+    os << "Block idx: " << idx << " ";
+    os << std::hex;
+    for(size_t i = 0; i < rp_ProcessingChank->GetSize(); i++) {
+        os << rp_ProcessingChank->GetData()[i];
+    }
+    os << std::dec << std::endl;
+}
--- FileWriter.h
+++ FileWriter.h
@@ -5,4 +5,53 @@
 #ifndef SIGNATURE_FILEWRITER_H
 #define SIGNATURE_FILEWRITER_H
 
+#include <vector>
+#include <bits/unique_ptr.h>
+#include <mutex>
+#include "DataChank.h"
+
+namespace Signature {
+
+    /**
+     * Represent object, which write block hashes to output file
+     */
+    class FileWriter {
+    public:
+        /**
+         * Default constructor
+         *
+         * @param hashCount     count waited hashes
+         * @param path          path to output file
+         */
+        FileWriter(size_t hashCount, std::string path);
+
+        /**
+         * Thread function, which waits and prints hashes to output
+         */
+        void ProcessHash();
+
+    private:
+
+        /**
+         * Print hash to output
+         * @param os                        output stream
+         * @param rp_ProcessingChank        reference on unique pointer which points on hash chank
+         * @param idx                       hash block id
+         */
+        void PrintToOutput(std::ostream& os, std::unique_ptr<DataChank>& rp_ProcessingChank, size_t idx);
+
+        /// Path to output file
+        ///
+        std::string m_Path;
+
+        /// Collection, which contains unique pointers on hash data chanks
+        ///
+        std::vector<std::unique_ptr<DataChank>> m_HashData;
+
+        /// Mutex for m_HashData guarding
+        ///
+        std::mutex m_WriteMutex;
+    };
+}
+
 #endif //SIGNATURE_FILEWRITER_H
